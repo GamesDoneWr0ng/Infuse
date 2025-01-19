1cd95d818cd99d06d7c992152595f8797065b008
GamesDoneWr0ng <ask@flaten.no>
2025-01-19 19:35:56 +0100

    disable fire aspect

--- src/main/java/org/gamesdonewr0ng/infuse/mixin/DealDamageMixin.java
+++ src/main/java/org/gamesdonewr0ng/infuse/mixin/DealDamageMixin.java
@@ -1,27 +1,39 @@
 package org.gamesdonewr0ng.infuse.mixin;
 
+import net.minecraft.component.type.ItemEnchantmentsComponent;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.gamesdonewr0ng.infuse.DataHandler;
 import org.gamesdonewr0ng.infuse.util.IEntityDataSaver;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Objects;
+import java.util.*;
 
 @Mixin(ServerPlayerEntity.class)
 public class DealDamageMixin {
     @Inject(method = "attack", at = @At("HEAD"))
     private void attack(Entity target, CallbackInfo info) {
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
+        ItemStack weapon = player.getMainHandStack();
+
+        if (EnchantmentHelper.getLevel(player.getWorld().getRegistryManager().getWrapperOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.FIRE_ASPECT), weapon) != 0) {
+            removeFireAspect(weapon);
+        }
+
         String primary = DataHandler.getPrimary((IEntityDataSaver) player);
         if (primary.equals("Lightning")) {
             // check critical and random
@@ -65,5 +77,15 @@ public class DealDamageMixin {
         if (DataHandler.getSupport((IEntityDataSaver) player).equals("Fire")) {
             target.setOnFireFor(4);
         }
+    }
+
+    @Unique
+    private void removeFireAspect(ItemStack weapon) {
+        ItemEnchantmentsComponent enchantments = EnchantmentHelper.getEnchantments(weapon);
+
+        ItemEnchantmentsComponent.Builder builder = new ItemEnchantmentsComponent.Builder(enchantments);
+        builder.remove(entry -> entry.matchesKey(Enchantments.FIRE_ASPECT));
+
+        EnchantmentHelper.set(weapon, builder.build());
     }
 }
